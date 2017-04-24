695723d84816c5fb1f864ac688b43195c577fec5
henry <seedtree.henry@gmail.com>
2017-04-24 20:18:29 +1000

    add postings model and controller

---
+++ db/migrate/20170424100019_create_postings.rb
@@ -0,0 +1,9 @@
+class CreatePostings < ActiveRecord::Migration[5.0]
+  def change
+    create_table :postings do |t|
+      t.string  :title
+      t.text    :body
+      t.timestamps
+    end
+  end
+end
